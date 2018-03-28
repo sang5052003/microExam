a28c153a8590f8c657f644639f72b532c93e87b3
sang5052003 <sang5052003@rosesystems.kr>
2018-03-28 16:57:09 +0900

    db_config edit for container run.bat script add - auto excute for js files

--- chapter5/dbConfig.js
+++ chapter5/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: process.env.DB_HOST,
+    host: '192.168.1.93', //bridge network
     port: 3306,
     user: 'sang5052003',
     password: 'sang5052003',
-    database: 'micro'
+    database: 'microdb'
 };
